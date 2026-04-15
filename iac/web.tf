5d12e060aec1f920b333654afbd15a80dd82e683
Rony Fabrizio Lujan Holguin <152334517+ronyjuan@users.noreply.github.com>
2026-04-15 14:22:43 +0000

    feat(iac): implementar redes de datos y aislamiento de contenedores

--- iac/web.tf
+++ iac/web.tf
@@ -1,33 +1,34 @@
-resource "docker_container" "web" {
-  name  = "web-${terraform.workspace}-01"
-  image = "lab/web"
-
-   ports {
-    internal = "80"
-    external = var.web_port[terraform.workspace]
-  }
-}
-
-resource "docker_container" "api" {
-  name  = "api-${terraform.workspace}-01"
-  image = "lab/api"
-
-   ports {
-    internal = "3000"
-    external = var.api_port[terraform.workspace]
-  }
+resource "docker_network" "app_network" {
+  name = "net-${terraform.workspace}"
 }
 
 resource "docker_container" "bd" {
   name  = "bd-${terraform.workspace}"
   image = "mysql:8.0"
-
+  networks_advanced { name = docker_network.app_network.name }
   ports {
     internal = "3306"
     external = var.bd_port[terraform.workspace]
   }
+  env = ["MYSQL_ROOT_PASSWORD=root"]
+}
 
-  env = [
-    "MYSQL_ROOT_PASSWORD=root"
-  ]
+resource "docker_container" "api" {
+  name  = "api-${terraform.workspace}-01"
+  image = "lab/api"
+  networks_advanced { name = docker_network.app_network.name }
+  ports {
+    internal = "3000"
+    external = var.api_port[terraform.workspace]
+  }
+}
+
+resource "docker_container" "web" {
+  name  = "web-${terraform.workspace}-01"
+  image = "lab/web"
+  networks_advanced { name = docker_network.app_network.name }
+  ports {
+    internal = "80"
+    external = var.web_port[terraform.workspace]
+  }
 }
